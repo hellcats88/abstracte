7a3b43477461efc28f914c973c828d17f0d7da5b
gatti88 <gatti88@libero.it>
2021-03-09 09:40:23 +0100

    Added pkcs11 aws cloudhsm

--- security/aws/cloudhsm/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/hellcats88/abstracte/security/aws/cloudhsm
-
-go 1.15
--- storage/transaction.go
+++ storage/transaction.go
@@ -1,5 +1,7 @@
 package storage
 
+import "errors"
+
 type Transaction interface {
 	Ref() interface{}
 	Commit() error
@@ -7,3 +9,17 @@ type Transaction interface {
 	Begin() (Transaction, error)
 	Query() interface{}
 }
+
+// Context is a generic definition for an SQL connection state
+type Context interface {
+	ConnStr() string
+	Closed() bool
+	Open() error
+	Close()
+	Tx() (Transaction, error)
+	UnmanagedTx() (Transaction, error)
+}
+
+var ErrAlreadyExists = errors.New("Duplicated key detected")
+var ErrNotFound = errors.New("Record not found")
+var ErrTooManyRecordFound = errors.New("Requesting single record returns more than one")
