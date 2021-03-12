1b11a5fa0085a074ea83ef67660c6f8b8a823d8c
gatti88 <gatti88@libero.it>
2021-03-12 10:31:43 +0100

    extra parameters for route and routegroup

--- api/http.go
+++ api/http.go
@@ -2,7 +2,7 @@ package api
 
 type Http interface {
 	AddGroup(name string, subPath string, config ConfigGroup) error
-	AddRoute(path string, config ConfigRoute, service Service) error
-	AddGroupRoute(path string, group string, config ConfigRoute, service Service)
+	AddRoute(method string, path string, config ConfigRoute, service Service) error
+	AddGroupRoute(method string, path string, group string, config ConfigRoute, service Service)
 	Listen(port int, address string)
 }
