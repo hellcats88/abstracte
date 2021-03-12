d7aeba508711d84e58bc399d06a1761fe56fa124
gatti88 <gatti88@libero.it>
2021-03-12 09:36:18 +0100

    Defined api group config

--- api/http.go
+++ api/http.go
@@ -1,7 +1,7 @@
 package api
 
 type Http interface {
-	AddGroup(name string, subPath string, config ApiConfig) error
+	AddGroup(name string, subPath string, config ApiConfigGroup) error
 	AddRoute(path string)
 	AddGroupRoute(path string, group string)
 	Listen(port int, address string)
