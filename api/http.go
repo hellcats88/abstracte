84d62fba21fd283b37739df23dbe9bd9ca3e858a
gatti88 <gatti88@libero.it>
2021-03-12 10:56:14 +0100

    Fixed missing error return value

--- api/http.go
+++ api/http.go
@@ -3,6 +3,6 @@ package api
 type Http interface {
 	AddGroup(name string, subPath string, config ConfigGroup) error
 	AddRoute(method string, path string, config ConfigRoute, service Service) error
-	AddGroupRoute(method string, path string, group string, config ConfigRoute, service Service)
+	AddGroupRoute(method string, path string, group string, config ConfigRoute, service Service) error
 	Listen(port int, address string) error
 }
