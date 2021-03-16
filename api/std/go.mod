03194f76888cbd4f59aab18d8ba673c45d0ee9dc
gatti88 <gatti88@libero.it>
2021-03-16 18:11:53 +0100

    Partial version of std http api

--- api/std/go.mod
+++ api/std/go.mod
@@ -4,5 +4,5 @@ go 1.15
 
 require (
 	github.com/gin-gonic/gin v1.6.3
-	github.com/hellcats88/abstracte v0.0.0-20210312100044-d21e1159bd31
+	github.com/hellcats88/abstracte v0.0.0-20210316094931-e2ddeb586c1d
 )
--- api/std/std.go
+++ api/std/std.go
@@ -1,83 +1,130 @@
 package std
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"github.com/hellcats88/abstracte/api"
 	"github.com/hellcats88/abstracte/logging"
 	"github.com/hellcats88/abstracte/tenant"
 )
 
+type handlerCtx map[string]interface{}
+
+type handler func(handlerCtx, http.ResponseWriter, *http.Request, *handlerInfo) bool
+
+type handlerInfo struct {
+	h    handler
+	next *handlerInfo
+}
+
+func Next(ctx handlerCtx, w http.ResponseWriter, req *http.Request, info *handlerInfo) bool {
+	if info != nil {
+		return info.h(ctx, w, req, info.next)
+	}
+
+	return true
+}
+
+func array_interface2Handler(arr []interface{}) []handler {
+	var result []handler
+	for _, item := range arr {
+		result = append(result, item.(handler))
+	}
+
+	return result
+}
+
+type group struct {
+	name     string
+	handlers []handler
+}
+
+type route struct {
+	log      logging.Logger
+	method   string
+	handlers []handlerInfo
+	service  api.Service
+	ctx      handlerCtx
+}
+
+func (rt route) handle(w http.ResponseWriter, r *http.Request) {
+	if rt.method != r.Method {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	rt.handlers[0].h(rt.ctx, w, r, rt.handlers[0].next)
+}
+
 type stdHttp struct {
-	engine    *gin.Engine
 	log       logging.Logger
-	groups    map[string]*gin.RouterGroup
+	groups    map[string]group
 	tenantCtx tenant.Context
 	logCtx    logging.Context
 }
 
 func New(log logging.Logger) api.Http {
-	engine := gin.Default()
-	return ginHttp{
-		engine: engine,
+	return stdHttp{
 		log:    log,
-		groups: make(map[string]*gin.RouterGroup),
+		groups: make(map[string]group),
 	}
 }
 
-func (g *ginHttp) wrapService(service api.Service) gin.HandlerFunc {
-	return func(c *gin.Context) {
+func (g *stdHttp) wrapService(service api.Service) {
 
-	}
 }
 
-func (g *ginHttp) definePreBehavior(config api.Config) []gin.HandlerFunc {
-	var handlers []gin.HandlerFunc
-	handlers = append(handlers, func(c *gin.Context) { g.createLogContextFromHeaders(c) })
+func (g *stdHttp) defineBehavior(config api.Config) []handler {
+	var handlers []handler
+	handlers = append(handlers, g.createLogContextFromHeaders)
 
 	switch config.Tenant {
 	case api.ConfigTenantFromHeaders:
-		handlers = append(handlers, func(c *gin.Context) { g.retrieveTenant(c) })
+		handlers = append(handlers, g.retrieveTenant)
 	}
 
 	return handlers
 }
 
-func (g *ginHttp) definePostBehavior(config api.Config) []gin.HandlerFunc {
-	var handlers []gin.HandlerFunc
-	return handlers
-}
-
-func (g *ginHttp) retrieveTenant(ctx *gin.Context) {
+func (g *stdHttp) retrieveTenant(ctx handlerCtx, w http.ResponseWriter, r *http.Request, info *handlerInfo) bool {
 	tCtx := tenant.Context{
-		Id:     ctx.GetHeader("X-Tenant-ID"),
-		UserId: ctx.GetHeader("X-Tenant-UserID"),
+		Id:     r.Header.Get("X-Tenant-ID"),
+		UserId: r.Header.Get("X-Tenant-UserID"),
 	}
 
 	if tCtx.Id == "" || tCtx.UserId == "" {
 		g.log.Error(g.logCtx, "Rejected request caused by missing tenant informations")
 
-		ctx.AbortWithStatusJSON(http.StatusUnauthorized, api.Model{
+		bt, _ := json.Marshal(api.Model{
 			Error: api.ErrorModel{
 				Code:   api.ApiErrorAuthFailed,
 				Msg:    "Failed to get user information",
 				DevMsg: "Missing X-Tenant-ID or X-Tenant-UserID headers",
 			},
 		})
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusUnauthorized)
+
+		if _, err := w.Write(bt); err != nil {
+			g.log.Error(g.logCtx, "Failed to write response not authorized to client. %v", err)
+		}
+
+		return false
 	}
 
 	g.tenantCtx = tCtx
 
-	ctx.Set(api.TenantKey, tCtx)
-	ctx.Next()
+	ctx[api.TenantKey] = tCtx
+	return Next(ctx, w, r, info)
 }
 
-func (g *ginHttp) createLogContextFromHeaders(ctx *gin.Context) {
+func (g *stdHttp) createLogContextFromHeaders(ctx handlerCtx, w http.ResponseWriter, r *http.Request, info *handlerInfo) bool {
 	var lCtx logging.Context
 
-	if corrId := ctx.GetHeader("X-Correlation-ID"); corrId != "" {
+	if corrId := r.Header.Get("X-Correlation-ID"); corrId != "" {
 		lCtx = logging.NewContext(corrId)
 	} else {
 		lCtx = logging.NewContextUUID()
@@ -85,50 +132,95 @@ func (g *ginHttp) createLogContextFromHeaders(ctx *gin.Context) {
 
 	g.logCtx = lCtx
 
-	ctx.Set(api.LogKey, lCtx)
-	ctx.Next()
+	ctx[api.LogKey] = lCtx
+	return Next(ctx, w, r, info)
 }
 
-func (g ginHttp) AddGroup(name string, subPath string, config api.ConfigGroup) error {
+func (g stdHttp) AddGroup(name string, subPath string, config api.ConfigGroup) error {
 	if _, exists := g.groups[name]; exists {
 		return fmt.Errorf("Group %s already exists, skipping creation", name)
 	}
 
-	var handlers []gin.HandlerFunc
-	handlers = append(handlers, g.definePreBehavior(config.Config)...)
-	handlers = append(handlers, array_interface2HandlerFunc(config.CustomHandlers)...)
-	handlers = append(handlers, g.definePostBehavior(config.Config)...)
+	var handlers []handler
+	handlers = append(handlers, g.defineBehavior(config.Config)...)
+	handlers = append(handlers, array_interface2Handler(config.Handlers)...)
 
-	g.groups[name] = g.engine.Group(subPath, handlers...)
+	g.groups[name] = group{
+		name:     name,
+		handlers: handlers,
+	}
 
 	return nil
 }
 
-func (g ginHttp) AddGroupRoute(method string, path string, group string, config api.ConfigRoute, service api.Service) error {
+func (g stdHttp) AddGroupRoute(method string, path string, group string, config api.ConfigRoute, service api.Service) error {
 	groupRef, exists := g.groups[group]
 	if !exists {
 		return fmt.Errorf("Group %s does not exists, skipping route %s creation", group, path)
 	}
 
-	var handlers []gin.HandlerFunc
-	handlers = append(handlers, g.definePreBehavior(config.Config)...)
-	handlers = append(handlers, g.wrapService(service))
-	handlers = append(handlers, g.definePostBehavior(config.Config)...)
+	http.HandleFunc(path, func(rw http.ResponseWriter, r *http.Request) {
+		var handlers []handler
+		handlers = append(handlers, groupRef.handlers...)
+		handlers = append(handlers, g.defineBehavior(config.Config)...)
+		handlers = append(handlers, array_interface2Handler(config.CustomHandlers)...)
 
-	groupRef.Handle(method, path, handlers...)
+		var hInfo []handlerInfo
+
+		for _, h := range handlers {
+			hInfo = append(hInfo, handlerInfo{
+				h: h,
+			})
+		}
+
+		//last handler have next = nil so we can cycle to len - 1
+		for i := 0; i < len(hInfo)-1; i++ {
+			hInfo[i].next = &hInfo[i+1]
+		}
+
+		route{
+			log:      g.log,
+			method:   method,
+			handlers: hInfo,
+			service:  service,
+			ctx:      make(handlerCtx),
+		}.handle(rw, r)
+	})
+
 	return nil
 }
 
-func (g ginHttp) AddRoute(method string, path string, config api.ConfigRoute, service api.Service) error {
-	var handlers []gin.HandlerFunc
-	handlers = append(handlers, g.definePreBehavior(config.Config)...)
-	handlers = append(handlers, g.wrapService(service))
-	handlers = append(handlers, g.definePostBehavior(config.Config)...)
+func (g stdHttp) AddRoute(method string, path string, config api.ConfigRoute, service api.Service) error {
+	var handlers []handler
+	handlers = append(handlers, g.defineBehavior(config.Config)...)
+	handlers = append(handlers, array_interface2Handler(config.CustomHandlers)...)
 
-	g.engine.Handle(method, path, handlers...)
+	var hInfo []handlerInfo
+
+	for _, h := range handlers {
+		hInfo = append(hInfo, handlerInfo{
+			h: h,
+		})
+	}
+
+	//last handler have next = nil so we can cycle to len - 1
+	for i := 0; i < len(hInfo)-1; i++ {
+		hInfo[i].next = &hInfo[i+1]
+	}
+
+	http.HandleFunc(path, func(rw http.ResponseWriter, r *http.Request) {
+		route{
+			log:      g.log,
+			method:   method,
+			handlers: hInfo,
+			service:  service,
+			ctx:      make(handlerCtx),
+		}.handle(rw, r)
+	})
+
 	return nil
 }
 
-func (g ginHttp) Listen(port int, address string) error {
-	return g.engine.Run(fmt.Sprintf("%s:%d", address, port))
+func (g stdHttp) Listen(port int, address string) error {
+	return http.ListenAndServe(fmt.Sprintf("%s:%d", address, port), nil)
 }
