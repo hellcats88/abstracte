d8f708542b8bc3a385ed9de23932aafef92c6df7
gatti88 <gatti88@libero.it>
2021-03-29 08:29:54 +0200

    Defined service input/output interfaces

--- api/service.go
+++ api/service.go
@@ -2,11 +2,14 @@ package api
 
 import (
 	"github.com/hellcats88/abstracte/runtime"
-	"github.com/hellcats88/abstracte/tenant"
 )
 
 type ServiceInput interface {
-	TenantCtx() tenant.Context
+	Raw() interface{}
+	RuntimeCtx() runtime.Context
+	Model() interface{}
+	InputParams() interface{}
+	QueryParams() interface{}
 }
 
 type ServiceOutput interface {
